74b06dcfca3a9c38b7783a4bb8caa00486204d15
douglarek <douglarek@gmail.com>
2023-02-04 22:17:04 +0800

    Enable all tree sitter

--- lua/nvim-treesitter_config.lua
+++ lua/nvim-treesitter_config.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all"
-	ensure_installed = { "lua", "go" },
+	ensure_installed = "all",
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
@@ -9,7 +9,7 @@ require 'nvim-treesitter.configs'.setup {
 	auto_install = true,
 
 	-- List of parsers to ignore installing (for "all")
-	ignore_install = { "javascript" },
+	ignore_install = {},
 
 	highlight = {
 		-- `false` will disable the whole extension
