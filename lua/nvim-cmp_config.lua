05c3bc004a47e883e86de543b1423060b6e29bfe
douglarek <douglarek@gmail.com>
2023-02-12 00:47:38 +0800

    Improve nvim cmp lsp

--- lua/nvim-cmp_config.lua
+++ lua/nvim-cmp_config.lua
@@ -11,90 +11,90 @@ local feedkey = function(key, mode)
 	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = {
-		['<C-p>'] = cmp.mapping.select_prev_item(),
-		['<C-n>'] = cmp.mapping.select_next_item(),
+    snippet = {
+        expand = function(args)
+	        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = {
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
 
-		-- BUG: https://github.com/hrsh7th/nvim-cmp/issues/1010#issuecomment-1142112534
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif vim.fn["vsnip#available"](1) == 1 then
-				feedkey("<Plug>(vsnip-expand-or-jump)", "")
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		['<S-Tab>'] = cmp.mapping(function()
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-				feedkey("<Plug>(vsnip-jump-prev)", "")
-			end
-		end, { "i", "s" }),
+        -- BUG: https://github.com/hrsh7th/nvim-cmp/issues/1010#issuecomment-1142112534
+        ["<Tab>"] = cmp.mapping(function(fallback)
+	        if cmp.visible() then
+		        cmp.select_next_item()
+	        elseif vim.fn["vsnip#available"](1) == 1 then
+		        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+	        else
+		        fallback()
+	        end
+        end, { "i", "s" }),
+        ['<S-Tab>'] = cmp.mapping(function()
+	        if cmp.visible() then
+		        cmp.select_prev_item()
+	        elseif vim.fn["vsnip#jumpable"]( -1) == 1 then
+		        feedkey("<Plug>(vsnip-jump-prev)", "")
+	        end
+        end, { "i", "s" }),
 
-		['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.close(),
-		['<CR>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Insert,
-			select = true,
-		})
-	},
-	sources = {
-		{ name = 'path' }, -- file paths
-		{ name = 'nvim_lsp', keyword_length = 2 }, -- from language server
-		{ name = 'nvim_lsp_signature_help' }, -- display function signatures with current parameter emphasized
-		{ name = 'nvim_lua', keyword_length = 2 }, -- complete neovim's Lua runtime API such vim.lsp.*
-		{ name = 'buffer', keyword_length = 2 }, -- source current buffer
-		{ name = 'vsnip', keyword_length = 2 }, -- nvim-cmp source for vim-vsnip
-		{ name = 'calc' }, -- source for math calculation
-	},
-	formatting = {
-		fields = { 'menu', 'abbr', 'kind' },
-		format = function(entry, item)
-			local menu_icon = {
-				nvim_lsp = 'λ',
-				vsnip = '⋗',
-				buffer = 'Ω',
-				path = '🖫',
-			}
-			item.menu = menu_icon[entry.source.name]
-			return item
-		end,
-	},
+        ['<C-S-f>'] = cmp.mapping.scroll_docs( -4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        })
+    },
+    sources = {
+        { name = 'path' }, -- file paths
+        { name = 'nvim_lsp',               keyword_length = 1 }, -- from language server
+        { name = 'nvim_lsp_signature_help' }, -- display function signatures with current parameter emphasized
+        { name = 'nvim_lua',               keyword_length = 2 }, -- complete neovim's Lua runtime API such vim.lsp.*
+        { name = 'buffer',                 keyword_length = 2 }, -- source current buffer
+        { name = 'vsnip',                  keyword_length = 2 }, -- nvim-cmp source for vim-vsnip
+        { name = 'calc' }, -- source for math calculation
+    },
+    formatting = {
+        fields = { 'menu', 'abbr', 'kind' },
+        format = function(entry, item)
+	        local menu_icon = {
+	            nvim_lsp = 'λ',
+	            vsnip = '⋗',
+	            buffer = 'Ω',
+	            path = '🖫',
+	        }
+	        item.menu = menu_icon[entry.source.name]
+	        return item
+        end,
+    },
 })
 
 cmp.setup.filetype('gitcommit', {
-	sources = cmp.config.sources({
-		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-	}, {
-		{ name = 'buffer' },
-	})
+    sources = cmp.config.sources({
+        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+        { name = 'buffer' },
+    })
 })
 
 cmp.setup.cmdline('/', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = 'buffer' }
-	}
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
 })
 
 cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	}, {
-		{ name = 'cmdline' }
-	})
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
 })
