5b83bc19fffd8d8e37fa0108bb5d9e469a107036
douglarek <douglarek@gmail.com>
2023-04-07 15:34:13 +0800

    predownload and ignore ruby

--- lua/nvim-treesitter_config.lua
+++ lua/nvim-treesitter_config.lua
@@ -1,3 +1,4 @@
+require("nvim-treesitter.install").prefer_git = false
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
   ensure_installed = "all",
@@ -9,18 +10,16 @@ require 'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   -- List of parsers to ignore installing (for "all")
-  ignore_install = {},
+  ignore_install = { "ruby" },
 
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-
     -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
     disable = {},
-
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
