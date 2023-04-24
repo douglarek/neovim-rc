17ebc5a130bafc317ef5588929ea2818672f801a
douglarek <douglarek@gmail.com>
2023-04-24 13:59:23 +0800

    lua lsp and nvim tree migration

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -97,15 +97,29 @@ require("rust-tools").setup({
 -- lua
 vim.cmd('autocmd FileType lua setlocal et sta sw=2 sts=2')
 require("neodev").setup({})
-nvim_lsp.lua_ls.setup({
+-- see: https://github.com/LuaLS/lua-language-server, https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+nvim_lsp.lua_ls.setup {
   settings = {
     Lua = {
-      completion = {
-        callSnippet = "Replace"
-      }
-    }
-  }
-})
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 -- typescript
 vim.cmd('autocmd FileType typescript setlocal et sta sw=2 sts=2')
--- lua/nvim-treesitter_config.lua
+++ lua/nvim-treesitter_config.lua
@@ -1,7 +1,7 @@
 require 'nvim-treesitter.install'.compilers = { 'zig' }
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "lua", "go", "rust", "sql", "yaml" },
+  ensure_installed = { "lua", "go", "rust", "sql", "yaml", "python", "toml" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
